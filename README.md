67c5d6d19dd3f94f3813e1a76ddcdbf8d891b01a
JoYongHyeon <whdydgus5778@gmail.com>
2025-08-25 10:16:30 +0900

    README.md update

--- README.md
+++ README.md
@@ -29,3 +29,9 @@
 
 > 개발은 결국 **기록 싸움**.  
 > 남들이 보기 위한 게 아니라, **나한테 도움이 되는 흔적**을 남기는 게 목적.
+
+
+## 참고 강의
+- [김영한의 실전 자바 - 고급 1편](https://www.inflearn.com/course/%EA%B9%80%EC%98%81%ED%95%9C%EC%9D%98-%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94-%EA%B3%A0%EA%B8%89-1/dashboard)
+- [김영한의 실전 자바 - 고급 2편](https://www.inflearn.com/course/%EA%B9%80%EC%98%81%ED%95%9C%EC%9D%98-%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94-%EA%B3%A0%EA%B8%89-2/dashboard)
+- [김영한의 실전 자바 - 고급 3편](https://www.inflearn.com/course/%EA%B9%80%EC%98%81%ED%95%9C%EC%9D%98-%EC%8B%A4%EC%A0%84-%EC%9E%90%EB%B0%94-%EA%B3%A0%EA%B8%89-3/dashboard)
---
+++ src/thread/DaemonThreadMain.java
@@ -0,0 +1,30 @@
+package thread;
+
+public class DaemonThreadMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        DaemonThread daemonThread = new DaemonThread();
+        // 데몬 스레드로 설정
+        // 데몬 스레드 여부는 start() 실행 전에 결정해야 한다. 이후에는 변경되지 않음
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+
+    static class DaemonThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": run() start");
+
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(Thread.currentThread().getName() + ": run() end");
+        }
+    }
+}
