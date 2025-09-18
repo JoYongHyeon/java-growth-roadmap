08a0b646e70d8c4bb1ae293af8a627cd3e062434
JoYongHyeon <whdydgus5778@gmail.com>
2025-09-18 17:54:25 +0900

    Codility Lesson 4 - MaxCounters: initial solution (77% efficiency)

---
+++ src/algorithm/codility/lesson_4/MaxCounters_3.java
@@ -0,0 +1,43 @@
+package algorithm.codility.lesson_4;
+
+import java.util.Arrays;
+
+public class MaxCounters_3 {
+
+    public static void main(String[] args) {
+
+        int N = 5;
+        int[] K = new int[N];
+        int[] A = {3, 4, 4, 6, 1, 4, 4};
+        int M = A.length;
+        int maxNum = 0;
+
+
+        // {0, 0, 0, 0, 0}
+        for (int i = 0; i < M; i++) {
+            if (1 <= A[i] && A[i] <= N) {
+                int i1 = A[i]; // 3
+                K[i1-1]++;
+                maxNum = Math.max(maxNum, K[i1 - 1]);
+            }
+
+            if (A[i] == (N + 1)) {
+                for (int j = 0; j < N; j++) {
+                    K[j] = maxNum;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(K));
+
+        /**
+         * 각 연산을 순서대로 적용하면 카운터 값의 변화는 다음과 같습니다:
+         * (0, 0, 1, 0, 0)
+         * (0, 0, 1, 1, 0)
+         * (0, 0, 1, 2, 0)
+         * (2, 2, 2, 2, 2)
+         * (3, 2, 2, 2, 2)
+         * (3, 2, 2, 3, 2)
+         * (3, 2, 2, 4, 2)
+         */
+    }
+}
