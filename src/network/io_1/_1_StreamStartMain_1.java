d6f76ddcb9ad9b2f51f61f89894b198b4e65347b
JoYongHyeon <whdydgus5778@gmail.com>
2025-10-10 10:03:19 +0900

    network i/o test code

---
+++ src/network/io_1/_1_StreamStartMain_1.java
@@ -0,0 +1,28 @@
+package network.io_1;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class _1_StreamStartMain_1 {
+
+    public static void main(String[] args) throws IOException {
+        // 파일에 데이터를 출력하는 스트림
+        FileOutputStream fos = new FileOutputStream("src/network/temp/hello.dat");
+        // byte 단위로 값을 출력
+        fos.write(65);
+        fos.write(66);
+        fos.write(67);
+        fos.close();
+
+        // 파일에서 데이터를 읽어오는 스트림
+        FileInputStream fis = new FileInputStream("src/network/temp/hello.dat");
+        // 파일에서 데이터를 byte 단위로 하나씩 읽음.
+        System.out.println(fis.read());
+        System.out.println(fis.read());
+        System.out.println(fis.read());
+        // 값이 없다면(끝에 도달) -1
+        System.out.println(fis.read());
+        fis.close();
+    }
+}
---
+++ src/network/io_1/_4_StreamStartMain_4.java
@@ -0,0 +1,23 @@
+package network.io_1;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class _4_StreamStartMain_4 {
+
+    public static void main(String[] args) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream("src/network/temp/hello.dat");
+        byte[] input = {65, 66, 67};
+        fos.write(input);
+        fos.close();
+
+        FileInputStream fis = new FileInputStream("src/network/temp/hello.dat");
+        // 스트림이 끝날 때 까지 모든 데이터를 한번에 읽어옴
+        byte[] readBytes = fis.readAllBytes();
+        System.out.println(Arrays.toString(readBytes));
+        fis.close();
+    }
+}
