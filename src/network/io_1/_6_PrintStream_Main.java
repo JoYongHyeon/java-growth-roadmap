9499b955371aad754f94f1eb1c84aba85f630d3f
JoYongHyeon <whdydgus5778@gmail.com>
2025-10-15 19:56:17 +0900

    key chain change test

--- src/network/io_1/_6_PrintStream_Main.java
+++ src/network/io_1/_6_PrintStream_Main.java
@@ -11,7 +11,6 @@ public class _6_PrintStream_Main {
 
         PrintStream printStream = System.out;
 
-        // test
         byte[] bytes = "Hello!\n".getBytes(UTF_8);
         printStream.write(bytes);
         printStream.println("Print!");
